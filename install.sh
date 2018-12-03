08be727b9ceb776fca70a18b1dd7857e29278aa3
Liby Lee <liby99@icloud.com>
2018-12-02 18:02:16 -0800

    More robust install

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Get the current bash file directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# First remove the existing files
+if [ ! -f ~/.vimrc ]; then
+  read -e -p "Detected ~/.vimrc file. Remove it? [Y/n] " YN
+  if [[ $YN == "y" || $YN == "Y" || $YN == "" ]]; then
+    echo "Removing ~/.vimrc"
+    rm ~/.vimrc
+  else
+    echo "Aborting"
+    exit 1
+  fi
+fi
+
+if [ ! -f ~/.vim ]; then
+  read -e -p "Detected ~/.vim directory. Remove it? [Y/n] " YN
+  if [[ $YN == "y" || $YN == "Y" || $YN == "" ]]; then
+    echo "Removing ~/.vim"
+    rm -rf ~/.vim
+  else
+    echo "Aborting"
+    exit 1
+  fi
+fi
+
+# Then link the vim and vimrc
+ln -s "${DIR}/.vimrc" ~/.vimrc
+ln -s "${DIR}/.vim" ~/.vim

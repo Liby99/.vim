bf949534f2a2a4d0d0697f729a0707e5b1332160
Liby Lee <liby99@icloud.com>
2018-12-02 20:26:13 -0800

    More gitmodules

--- install.sh
+++ install.sh
@@ -3,30 +3,25 @@
 # Get the current bash file directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-# First remove the existing files
-if [ -e ~/.vimrc ]; then
-  read -e -p "Detected ~/.vimrc file. Remove it? [Y/n] " YN
-  if [[ $YN == "y" || $YN == "Y" || $YN == "" ]]; then
-    echo "Removing ~/.vimrc"
-    rm ~/.vimrc
-  else
-    echo "Aborting"
-    exit 1
+# For all the files
+for FILE in ".vimrc" ".vim";
+do
+  
+  # First remove the existing files
+  HOMEFILE=~/$FILE
+  if [ -e $HOMEFILE ]; then
+    read -e -p "Detected $HOMEFILE file. Remove it? [Y/n] " YN
+    if [[ $YN == "y" || $YN == "Y" || $YN == "" ]]; then
+      echo "Removing $HOMEFILE"
+      rm -rf $HOMEFILE
+    else
+      echo "Aborting"
+      exit 1
+    fi
   fi
-fi
-
-if [ -e ~/.vim ]; then
-  read -e -p "Detected ~/.vim directory. Remove it? [Y/n] " YN
-  if [[ $YN == "y" || $YN == "Y" || $YN == "" ]]; then
-    echo "Removing ~/.vim"
-    rm -rf ~/.vim
-  else
-    echo "Aborting"
-    exit 1
-  fi
-fi
-
-# Then link the vim and vimrc
-ln -s "${DIR}/.vimrc" ~/.vimrc
-ln -s "${DIR}/.vim" ~/.vim
-echo "Successfully linked .vimrc and .vim"
+  
+  # Then link the repo file to home file
+  CURRFILE="${DIR}/$FILE"
+  echo "Linking $FILE"
+  ln -s "$CURRFILE" $HOMEFILE 
+done
